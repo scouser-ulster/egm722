cee34907e8877a9e5b124784b1d9091a78744be2
scouser-ulster <couser-s2@ulster.ac.uk>
2025-03-18 20:36:22 +0000

    fix guessing game script
    adjusted rows 11 and 13 (flipped them around) to make it work

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -8,9 +8,9 @@ guess = int(input())  # number needs to be an integer
 
 while guess != rand:  # if the guess is not equal to the random number, you have to guess again
     if guess > rand:  # if the guess is too high, tell the user.
-        print('Too low. Guess again.')
-    else:  # if the guess is too low, tell the user.
         print('Too high. Guess again.')
+    else:  # if the guess is too low, tell the user.
+        print('Too low. Guess again.')
 
     print('Enter a new guess: ')
     guess = int(input())
